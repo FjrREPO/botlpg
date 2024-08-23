337425da026dfb9012e8031a16e1969fadc7261a
FjrREPO <jossfajar21@gmail.com>
2024-08-23 20:54:59 +0700

    add drive img

--- app/_components/BotlpgLocal.tsx
+++ app/_components/BotlpgLocal.tsx
@@ -35,14 +35,16 @@ export const BotlpgLocal = () => {
                         >
                             <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                                 <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                                    Local Bot
+                                    Download Local Bot
                                 </h3>
                                 <div className="text-base !m-0 !p-0 font-normal">
                                     <span className="text-slate-500 ">
                                         Silahkan klik card ini untuk download file bot.
                                     </span>
                                 </div>
-                                <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+                                <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500 items-center justify-center">
+                                    <Image width={110} height={100} src="https://res.cloudinary.com/dutlw7bko/image/upload/v1724421086/project-orang/drive-logo_m96f3d.png" alt="Google Drive" />
+                                </div>
                             </div>
                         </PinContainer>
                     </div>
